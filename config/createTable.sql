0325411ad4fade283683e80679f5a716902445c5
Your Name <you@example.com>
2021-04-29 11:41:57 +0300

    added createtable.sql

---
+++ config/createTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `data` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `longitude` varchar(128) NOT NULL,
+    `latitude`  varchar(128) NOT NULL,
+    `iodata` text NOT NULL,
+    PRIMARY KEY (`id`)
+);
